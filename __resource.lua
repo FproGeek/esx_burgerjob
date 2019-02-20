c8ae9efebbcf6ff7162393bc38415fe59190e220
FproGeek <41802608+FproGeek@users.noreply.github.com>
2019-02-20 21:40:30 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -7,18 +7,14 @@ version '1.1.0'
 
 client_scripts {
   '@es_extended/locale.lua',
-  'locales/en.lua',
   'locales/fr.lua',
-  'locales/sv.lua',
   'config.lua',
   'client/main.lua'
 }
 
 server_scripts {
   '@es_extended/locale.lua',
-  'locales/en.lua',
   'locales/fr.lua',
-  'locales/sv.lua',
   'config.lua',
   'server/main.lua'
 }
