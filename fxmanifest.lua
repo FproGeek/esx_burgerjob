6f7001a04278a24395c5552dda57c44e105b920b
FproGeek <41802608+FproGeek@users.noreply.github.com>
2020-04-11 19:04:28 +0200

    1.2.0 (es_extended 1.2.0)
    Compatible es_extended 1.2.0

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX BurgerJob'
+version '1.2.0'
+author 'FproGeek'
+
+client_scripts {
+  '@es_extended/locale.lua',
+  'locales/en.lua',
+  'locales/fr.lua',
+  'config.lua',
+  'client/main.lua'
+}
+
+server_scripts {
+  '@es_extended/locale.lua',
+  'locales/en.lua',
+  'locales/fr.lua',
+  'config.lua',
+  'server/main.lua'
+}
