395daec3eae2e056ec6dd8f8c0f0ee4096c7b971
FproGeek <41802608+FproGeek@users.noreply.github.com>
2020-04-11 16:31:14 +0200

    1.2.0

--- __resource.lua
+++ __resource.lua
@@ -1,8 +1,8 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'ESX Unicorn Job'
+description 'ESX BurgerJob'
 
-version '1.1.0'
+version '1.2.0'
 
 client_scripts {
   '@es_extended/locale.lua',
