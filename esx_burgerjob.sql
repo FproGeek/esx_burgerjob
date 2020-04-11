764d981db7ee2a5d1dafdf4978ab89b5b42732da
FproGeek <41802608+FproGeek@users.noreply.github.com>
2020-04-11 18:09:02 +0200

    1.2.0 Fixe

--- esx_burgerjob.sql
+++ esx_burgerjob.sql
@@ -1,31 +1,26 @@
-SET @job_name = 'burgershot';
-SET @society_name = 'society_burgershot';
-SET @job_Name_Caps = 'Burgershot';
-
-
 
 INSERT INTO `addon_account` (name, label, shared) VALUES
-  (@society_name, @job_Name_Caps, 1)
+  ('society_burgershot', 'Burgershot', 1)
 ;
 
 INSERT INTO `addon_inventory` (name, label, shared) VALUES
-  (@society_name, @job_Name_Caps, 1),
+  ('society_burgershot', 'Burgershot', 1),
   ('society_burgershot_fridge', 'Burgershot (frigo)', 1)
 ;
 
 INSERT INTO `datastore` (name, label, shared) VALUES 
-    (@society_name, @job_Name_Caps, 1)
+    ('society_burgershot', 'Burgershot', 1)
 ;
 
 INSERT INTO `jobs` (name, label) VALUES
-  (@job_name, @job_Name_Caps)
+  ('burgershot', 'Burgershot')
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-  (@job_name, 0, 'recruit', 'Recrue', 300, '{}', '{}'),
-  (@job_name, 1, 'employed', 'Employé', 300, '{}', '{}'),
-  (@job_name, 2, 'viceboss', 'Co-gérant', 500, '{}', '{}'),
-  (@job_name, 3, 'boss', 'Gérant', 600, '{}', '{}')
+  (burgershot, 0, 'recruit', 'Recrue', 300, '{}', '{}'),
+  (burgershot, 1, 'employed', 'Employé', 300, '{}', '{}'),
+  (burgershot, 2, 'viceboss', 'Co-gérant', 500, '{}', '{}'),
+  (burgershot, 3, 'boss', 'Gérant', 600, '{}', '{}')
 ;
 
 INSERT INTO `items` (`name`, `label`, `limit`) VALUES  
