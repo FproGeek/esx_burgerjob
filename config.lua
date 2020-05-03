d52802c5fd62ef91bc8733f3ea82b58768762eef
FproGeek <41802608+FproGeek@users.noreply.github.com>
2020-05-03 20:24:53 +0200

    2.0

--- config.lua
+++ config.lua
@@ -31,21 +31,32 @@ Config.Zones = {
         Pos   = { x = -1200.5029296875, y = -890.33709716797, z = 12.995155334473 },
         Size  = { x = 1.5, y = 1.5, z = 1.0 },
         Color = { r = 255, g = 187, b = 255 },
-        Type  = 1,
+        Name  = "Vestiaire",
+        Type  = 0
     },
 
     Vaults = {
         Pos   = { x = -1202.5881347656, y = -894.40582275391, z = 12.99516487126 },
         Size  = { x = 1.5, y = 1.5, z = 1.0 },
         Color = { r = 248, g = 248, b = 255 },
-        Type  = 1,
+        Name  = "Coffre",
+        Type  = 0
     },
 
     Fridge = {
         Pos   = { x = -1196.6790771484, y = -894.33947753906, z = 12.995160102844 },
         Size  = { x = 1.6, y = 1.6, z = 1.0 },
         Color = { r = 248, g = 248, b = 255 },
-        Type  = 1,
+        Name  = "Frigo",
+        Type  = 0
+    },
+
+    PreparationSteak = {
+        Pos   = { x = -1196.6790771484, y = -894.33947753906, z = 12.995160102844 },
+        Size  = { x = 1.6, y = 1.6, z = 1.0 },
+        Color = { r = 248, g = 248, b = 255 },
+        Name  = "Cuire steak",
+        Type  = 0
     },
 
     Vehicles = {
@@ -53,7 +64,8 @@ Config.Zones = {
         SpawnPoint   = { x = -1174.3291015625, y = -889.08239746094, z = 13.893969535828 },
         Size         = { x = 1.8, y = 1.8, z = 1.0 },
         Color        = { r = 255, g = 255, b = 0 },
-        Type         = 1,
+        Name  = "Garage véhicule",
+        Type  = 0,
         Heading      = 207.43,
     },
 
@@ -61,16 +73,41 @@ Config.Zones = {
         Pos   = { x = -1165.5532226563, y = -887.93658447266, z = 13.130926132202 },
         Size  = { x = 3.0, y = 3.0, z = 0.2 },
         Color = { r = 255, g = 255, b = 0 },
-        Type  = 1,
+        Name  = "Ranger le vehicule",
+        Type  = 0
     },
 
     BossActions = {
         Pos   = { x = -1194.7763671875, y = -900.59460449219, z = 12.99515914917 },
         Size  = { x = 1.5, y = 1.5, z = 1.0 },
         Color = { r = 0, g = 100, b = 0 },
-        Type  = 1,
+       Name  = "Menu patron",
+        Type  = 0
     },
 
+    KetchupFarm = {
+        Pos   = { x = 1190.064, y = -3106.073, z = 4.160 },
+        Size  = { x = 1.5, y = 1.5, z = 1.0 },
+        Color = { r = 255, g = 187, b = 255 },
+        Name  = "Récupérer marchandises",
+        Type  = 1
+    },
+
+    SachetKetchup = {
+        Pos   = { x = 2461.572, y = 1589.279, z = 32.000 },
+        Size  = { x = 1.5, y = 1.5, z = 1.0 },
+        Color = { r = 248, g = 248, b = 255 },
+        Name  = "Mise en sachet",
+        Type  = 1
+    },
+
+    SellFarm = {
+        Pos   = { x = 1719.426, y = 4759.067, z = 40.940 },
+        Size  = { x = 1.6, y = 1.6, z = 1.0 },
+        Color = { r = 248, g = 248, b = 255 },
+        Name  = "Vente",
+        Type  = 1
+    },
 
 -----------------------
 -------- SHOPS --------
@@ -79,7 +116,8 @@ Config.Zones = {
         Pos   = { x = -1179.884765625, y = -902.48187255859, z = 12.549982070922 },
         Size  = { x = 1.6, y = 1.6, z = 1.0 },
         Color = { r = 238, g = 0, b = 0 },
-        Type  = 23,
+        Name = "Boutique",
+        Type  = 0,
         Items = {
             { name = 'salads',      label = _U('salads'),   price = 3 },
             { name = 'tomater',      label = _U('tomater'),   price = 2 },
@@ -102,22 +140,23 @@ Config.Zones = {
 Config.Uniforms = {
   burgershot_outfit = {
    male = {
-        ['tshirt_1'] = 63,  ['tshirt_2'] = 0,
-        ['torso_1'] = 23,   ['torso_2'] = 3,
-        ['decals_1'] = 3,   ['decals_2'] = 0,
-        ['arms'] = 12,
-        ['pants_1'] = 24,   ['pants_2'] = 5,
-        ['shoes_1'] = 1,    ['shoes_2'] = 1,
+        ['tshirt_1'] = 15,  ['tshirt_2'] = 0,
+        ['torso_1'] = 281,   ['torso_2'] = 1,
+        ['decals_1'] = 0,   ['decals_2'] = 0,
+        ['arms'] = 24,
+        ['pants_1'] = 35,   ['pants_2'] = 0,
+        ['shoes_1'] = 26,    ['shoes_2'] = 1,
         ['chain_1'] = 0,    ['chain_2'] = 0
     },
         female = {
-        ['tshirt_1'] = 63,  ['tshirt_2'] = 0,
-        ['torso_1'] = 23,   ['torso_2'] = 3,
-        ['decals_1'] = 3,   ['decals_2'] = 0,
-        ['arms'] = 12,
-        ['pants_1'] = 24,   ['pants_2'] = 5,
-        ['shoes_1'] = 1,   ['shoes_2'] = 1,
-        ['chain_1'] = 0,  ['chain_2'] = 0
+        ['tshirt_1'] = 15,  ['tshirt_2'] = 0,
+        ['torso_1'] = 281,   ['torso_2'] = 1,
+        ['decals_1'] = 0,   ['decals_2'] = 0,
+        ['arms'] = 24,
+        ['pants_1'] = 35,   ['pants_2'] = 0,
+        ['shoes_1'] = 26,    ['shoes_2'] = 1,
+        ['chain_1'] = 0,    ['chain_2'] = 0
     }
   }
 }
+
